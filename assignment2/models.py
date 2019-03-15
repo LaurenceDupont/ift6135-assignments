0c0540060f7a4957db5959f34ad4b7326ffbe19c
marued <ruel.marcandre@gmail.com>
2019-03-14 21:06:45 -0400

    Assigment 2 - Using F.Softmax for Transformer

--- assignment2/models.py
+++ assignment2/models.py
@@ -533,14 +533,29 @@ class MultiHeadedAttention(nn.Module):
         self.fc_query = nn.Linear(self.n_units, n_heads * self.d_k)
         self.W_o = nn.Linear(n_units, n_units)
 
+        self.init_weights()
+
+    def init_weights(self):
+        k = np.sqrt(1.0 / self.n_units)
+
+        self.fc_keys.weight = nn.init.uniform_(self.fc_keys.weight, -k, k)
+        self.fc_keys.bias = nn.init.uniform_(self.fc_keys.bias, -k, k)
+
+        self.fc_values.weight = nn.init.uniform_(self.fc_values.weight, -k, k)
+        self.fc_values.bias = nn.init.uniform_(self.fc_values.bias, -k, k)
+
+        self.fc_query.weight = nn.init.uniform_(self.fc_query.weight, -k, k)
+        self.fc_query.bias = nn.init.uniform_(self.fc_query.bias, -k, k)
+
+        self.W_o.weight = nn.init.uniform_(self.W_o.weight, -k, k)
+        self.W_o.bias = nn.init.uniform_(self.W_o.bias, -k, k)
+
         # TODO: create/initialize any necessary parameters or layers
         # Note: the only Pytorch modules you are allowed to use are nn.Linear 
         # and nn.Dropout
-    def softmax(self, x, mask):
-        mask = mask.float().unsqueeze(1).expand(-1, x.shape[1], -1, -1) # expands on n_heads
-        x_tilde = (x * mask) - 1e9 * (1-mask)
-        x_tilde = x_tilde - torch.max(x)
-        x_tilde = torch.exp(x_tilde)
+    def softmax(self, x):
+        x = x - torch.max(x)
+        x_tilde = torch.exp(x)
         
         nb_dim = len(x_tilde.size())
         sumy = x_tilde.sum(0)
@@ -554,21 +569,19 @@ class MultiHeadedAttention(nn.Module):
 
     def attention(self, key, query, value, mask=None, dropout=None):
         "Compute 'Scaled Dot Product Attention'"
-        d_k = query.size(-1)
-        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
-        #if mask is not None:
-        #    scores = scores.transpose(0,1).masked_fill(mask == 0, -1e9).transpose(0,1)
-        #p_attn = F.softmax(scores, dim = -1)
-        p_attn = self.softmax(scores, mask)
+        x_tilde = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(self.d_k)
+        
+        if mask is not None:
+            mask = mask.float().unsqueeze(1).expand(-1, x_tilde.shape[1], -1, -1) # expands on n_heads
+            x_tilde = (x_tilde * mask) - 1e9 * (1-mask)
+        
+        p_attn = F.softmax(x_tilde, dim = -1)
+        #p_attn = self.softmax(x_tilde)
+        
         if dropout is not None:
             p_attn = dropout(p_attn)
+        
         return torch.matmul(p_attn, value)
-
-    # def attention(self, key, value, query, mask):
-    #     attention_values = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(self.d_k)
-    #     attention_values = self.softmax(attention_values, mask)
-    #     attention_values = self.dropout(attention_values)
-    #     return torch.matmul(attention_values, value)
 
     def forward(self, query, key, value, mask=None):
         # TODO: implement the masked multi-head attention.
