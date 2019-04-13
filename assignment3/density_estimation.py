5f636664e3b35a8d419d9d6d8651ca5f19726cd6
Vincent Morissette-Thomas <morv2604@gmail.com>
2019-04-13 17:14:13 -0400

    Question 1.1 JSD et debut du MLP Discriminator

--- assignment3/density_estimation.py
+++ assignment3/density_estimation.py
@@ -6,78 +6,77 @@ Created on Sat Mar 23 13:20:15 2019
 @author: chin-weihuang
 """
 
-
 from __future__ import print_function
 import numpy as np
-import torch 
+import torch
+from torch import nn
 import matplotlib.pyplot as plt
+from samplers import distribution4
 
-# plot p0 and p1
-plt.figure()
-
-# empirical
-xx = torch.randn(10000)
-f = lambda x: torch.tanh(x*2+1) + x*0.75
-d = lambda x: (1-torch.tanh(x*2+1)**2)*2+0.75
-plt.hist(f(xx), 100, alpha=0.5, density=1)
-plt.hist(xx, 100, alpha=0.5, density=1)
-plt.xlim(-5,5)
-# exact
-xx = np.linspace(-5,5,1000)
-N = lambda x: np.exp(-x**2/2.)/((2*np.pi)**0.5)
-plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-plt.plot(xx, N(xx))
+# Useful functions for plotting
+f = lambda x: torch.tanh(x * 2 + 1) + x * 0.75
+d = lambda x: (1 - torch.tanh(x * 2 + 1) ** 2) * 2 + 0.75
+N = lambda x: np.exp(-x ** 2 / 2.) / ((2 * np.pi) ** 0.5)
 
 
-############### import the sampler ``samplers.distribution4'' 
+def plot_discriminator():
+    '''
+    Plot the output of your trained discriminator and the estimated density contrasted with the true density
+    '''
+    # plot the output of your trained discriminator
+    xx = np.linspace(-5, 5, 1000)
+    r = xx  # evaluate xx using your discriminator; replace xx with the output
+    plt.figure(figsize=(8, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(xx, r)  # Not sure what this graph is
+    plt.title(r'$D(x)$')
+
+    # plot the estimated density contrasted with the true density
+    estimate = N(torch.from_numpy(xx)).numpy()  # estimate the density of distribution4 (on xx) using the discriminator;
+    plt.subplot(1, 2, 2)
+    plt.plot(xx, estimate)
+    plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy() ** (-1) * N(xx))
+    plt.legend(['Estimated', 'True'])
+    plt.title('Estimated vs True')
+    plt.savefig('Plots_Q1/Estimated_exact_D4')
+
+
+def plot_p0_p1():
+    '''
+    Plotting function
+    '''
+    plt.figure()
+    # empirical
+    xx = torch.randn(10000)
+    plt.hist(f(xx), 100, alpha=0.5, density=1)
+    plt.hist(xx, 100, alpha=0.5, density=1)
+    plt.xlim(-5, 5)
+    # exact
+    xx = np.linspace(-5, 5, 1000)
+    N = lambda x: np.exp(-x ** 2 / 2.) / ((2 * np.pi) ** 0.5)
+    plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy() ** (-1) * N(xx))
+    plt.plot(xx, N(xx))
+    plt.savefig('Plots_Q1/Empirical_Exact.png')
+
+
+def jsd(p, q):
+    '''
+    Implementation of pairwise Jensen Shannon Divergence
+    :param p: sequence Distribution p
+    :param q: sequence Distribution q
+    :return: float The calculated entropy
+    '''
+    p, q = np.asarray(p), np.asarray(q)
+    p, q = p / p.sum(), q / q.sum()  # Normalize the distribution p
+    r = (p + q) / 2.
+    return np.sum(p * np.log(p / r), axis=0) / 2. + np.sum(q * np.log(q / r), axis=0) / 2. + np.log(2)
+
 ############### train a discriminator on distribution4 and standard gaussian
 ############### estimate the density of distribution4
 
-#######--- INSERT YOUR CODE BELOW ---#######
- 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-############### plotting things
-############### (1) plot the output of your trained discriminator 
-############### (2) plot the estimated density contrasted with the true density
-
-
-
-r = xx # evaluate xx using your discriminator; replace xx with the output
-plt.figure(figsize=(8,4))
-plt.subplot(1,2,1)
-plt.plot(xx,r)
-plt.title(r'$D(x)$')
-
-estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator; 
-                                # replace "np.ones_like(xx)*0." with your estimate
-plt.subplot(1,2,2)
-plt.plot(xx,estimate)
-plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-plt.legend(['Estimated','True'])
-plt.title('Estimated vs True')
-
-
-
-
-
-
-
-
-
-
-
+class MLP_Discriminator(nn.Module):
+    def __init__(self):
+        super(MLP_Discriminator, self).__init__()
+        self.discriminator = nn.Sequential(
+            nn.Linear()
