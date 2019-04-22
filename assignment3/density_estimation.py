913319ed594255aa1ce13bde3e5faf290e7f4ad6
Vincent Morissette-Thomas <morv2604@gmail.com>
2019-04-22 17:30:37 -0400

    Cleaned up code for question 1

--- assignment3/density_estimation.py
+++ assignment3/density_estimation.py
@@ -1,20 +1,15 @@
-
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 Created on Sat Mar 23 13:20:15 2019
 @author: chin-weihuang
 """
-
-
 from __future__ import print_function
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-
 # plot p0 and p1
 plt.figure()
-
 # empirical
 xx = torch.randn(10000)
 f = lambda x: torch.tanh(x*2+1) + x*0.75
@@ -25,29 +20,24 @@ plt.xlim(-5,5)
 # exact
 xx = np.linspace(-5,5,1000)
 N = lambda x: np.exp(-x**2/2.)/((2*np.pi)**0.5)
-plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-plt.plot(xx, N(xx))
+plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx), 'b')
+plt.plot(xx, N(xx), color='orange')
+plt.legend(["f1(x)", "f0(x)"])
 plt.show()
-
 ############### import the sampler ``samplers.distribution4''
 ############### train a discriminator on distribution4 and standard gaussian
 ############### estimate the density of distribution4
-
 #######--- INSERT YOUR CODE BELOW ---#######
-
-=======
 import math
 import assignment3.samplers as samplers
 from torch.optim.sgd import SGD
-from assignment3.question1 import objectiveJS, Discriminator
+from assignment3.metric_estimator import objectiveJS, Discriminator
 from torch.utils.data.dataloader import DataLoader
-
 def new_objective(f1_validity, f0_validity):
     D_x = torch.mean(torch.log(f1_validity))
     D_y = torch.mean(torch.log(1.0 - f0_validity))
     objective = -1.0*(D_x + D_y)
     return objective
-
 def f1_density_estimator(x, discriminator, f0):
     """
     F1 density estimation function. Uses the question 5 procedure: f1(x) = f0(x) _dot_ D*(x)/(1-D*(x))
@@ -68,21 +58,15 @@ def f1_density_estimator(x, discriminator, f0):
     numerator = (np.multiply(f0_,output.squeeze()))
     denominator = (1.0-output.squeeze())
     return np.divide(numerator,denominator)
-
 p0_gen = iter(samplers.distribution3(batch_size=512))
 p0 = next(p0_gen)
-
 p1_gen = iter(samplers.distribution4(batch_size=512))
 p1 = next(p1_gen)
-
 epochs = 10000
-
 discriminator = Discriminator(input_size=1, hidden_size=[100, 300, 150])
 discriminator = discriminator.to('cuda:0')
 optimizer = SGD(discriminator.parameters(), lr=0.001)
 discriminator.train()
-
-
 for epoch in range(epochs):
     p0_Tensor = torch.Tensor(next(p0_gen)).to('cuda:0')
     p1_Tensor = torch.Tensor(next(p1_gen)).to('cuda:0')
@@ -96,9 +80,6 @@ for epoch in range(epochs):
     # Step
     objective.backward()
     optimizer.step()
-
-#print(-1.0*objective)
-
 xx_tensor = torch.Tensor(xx)
 xx_dl = DataLoader(xx_tensor, batch_size=512)
 output = torch.Tensor([]).to('cuda:0')
@@ -107,29 +88,23 @@ for xx_ in xx_dl:
     output = torch.cat((output, discriminator(xx_)), dim=0)
 output = output.cpu()
 output = output.detach().numpy()
-
-
-
-
 ############### plotting things
 ############### (1) plot the output of your trained discriminator 
 ############### (2) plot the estimated density contrasted with the true density
-
-
-
 r = output # evaluate xx using your discriminator; replace xx with the output
 plt.figure(figsize=(8,4))
 plt.subplot(1,2,1)
 plt.plot(xx,r.squeeze())
+plt.ylabel("Validity (%)")
+plt.xlabel("x")
 plt.title(r'$D(x)$')
-
 estimate = f1_density_estimator(xx, discriminator, N) # estimate the density of distribution4 (on xx) using the discriminator;
                                 # replace "np.ones_like(xx)*0." with your estimate
-
 plt.subplot(1,2,2)
 plt.plot(xx,estimate)
 plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
+plt.xlabel('x')
+plt.ylabel('f1 density')
 plt.legend(['Estimated','True'])
 plt.title('Estimated vs True')
 plt.show()
-
--- assignment3/metric_estimator.py
+++ assignment3/metric_estimator.py
@@ -6,20 +6,14 @@ import torch
 import matplotlib.pyplot as plt
 import assignment3.samplers as samplers
 from torch.optim.sgd import SGD
-from torch.utils.data.dataloader import DataLoader
-from torch.utils.data.dataset import TensorDataset
-
 # Code inspired from https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/gan/gan.py
-
 # 1.1 JSD Estimator
-
 class Discriminator(nn.Module):
     """
     Discriminator for the Jensen-Shannon Estimator
     """
     def __init__(self, input_size=2, hidden_size=None, output_size=1):
         super(Discriminator, self).__init__()
-
         if hidden_size is None:
             self.hidden_size = []
         else:
@@ -28,51 +22,23 @@ class Discriminator(nn.Module):
         self.input_size = input_size
         mlp = []
         input_size_ = input_size
-
         for i in range(len(self.hidden_size)):
             mlp.append(nn.Linear(input_size_, self.hidden_size[i]))
             mlp.append(nn.ReLU())
             input_size_ = self.hidden_size[i]
         mlp.append(nn.Linear(self.hidden_size[-1], self.output_size))
         mlp.append(nn.Sigmoid())
-
         self.mlp = nn.Sequential(*mlp)
-
     def forward(self, x):
         x = x.view(x.size(0), -1)
         validity = self.mlp(x)
         return validity
-
-
-class Generator(nn.Module):
-    def __init__(self, input_size=1, hidden_size=None, output_size=512):
-        super(Generator, self).__init__()
-        if hidden_size is None:
-            self.hidden_size = []
-        else:
-            self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.input_size = input_size
-        mlp = []
-        input_size_ = input_size
-
-        for i in range(len(self.hidden_size)):
-            mlp.append(nn.Linear(input_size_, self.hidden_size[i]))
-            mlp.append(nn.ReLU())
-            input_size_ = self.hidden_size[i]
-        mlp.append(nn.Linear(self.hidden_size[-1], self.output_size))
-
-        self.mlp = nn.Sequential(*mlp)
-
-    def forward(self, latent_variable):
-        gen_sample = self.mlp(latent_variable)
-        return gen_sample
-
-def objectiveJS(true_validity, gen_validity, device='cuda:0'):
+def objectiveJS(true_validity, gen_validity):
     """
     Objective to maximize (we're minimizing it's negative)
-    :param output:
-    :param target:
+    :param true_validity
+    :param gen_validity:
+    :param device: device used
     :return:
     """
     # We add a minus in front of the objective so that we can minimize it w/ grad descent
@@ -81,21 +47,19 @@ def objectiveJS(true_validity, gen_validity, device='cuda:0'):
     D_y = torch.mean(torch.log(1.0-gen_validity))
     objective = -1.0*math.log(2) - 0.5*(D_x+D_y)
     return objective
-
 def JensenShannonEstimator(true_distrib, gen_distrib, epochs=20):
     """
     JensenShannonEstimator
-    :param true_distrib:
-    :param gen_distrib:
-    :param lr:
-    :param epochs:
+    :param true_distrib: distribution learned as true
+    :param gen_distrib: distribution learned as generated
+    :param lr: learning rate
+    :param epochs: epochs used
     :return:
     """
     discriminator = Discriminator(input_size=2, hidden_size=[50, 100, 50])
     discriminator = discriminator.to('cuda:0')
-    optimizer = SGD(discriminator.parameters(), lr=0.1)
+    optimizer = SGD(discriminator.parameters(), lr=0.01)
     discriminator.train()
-
     for epoch in range(epochs):
         true_Tensor = torch.Tensor(next(true_distrib)).to('cuda:0')
         gen_Tensor = torch.Tensor(next(gen_distrib)).to('cuda:0')
@@ -109,16 +73,14 @@ def JensenShannonEstimator(true_distrib, gen_distrib, epochs=20):
         # Step
         objective.backward()
         optimizer.step()
-
-    # objective = np.mean(np.array(obj))
-    return -1.0*objective
-
-# 1.2 Wasserstein
-
+    return -objective
+# 1.2 Wasserstein Estimator
 class Critic(nn.Module):
+    """
+    Critic of the Wasserstein Estimator
+    """
     def __init__(self, input_size=2, hidden_size=None, output_size=1):
         super(Critic, self).__init__()
-
         if hidden_size is None:
             self.hidden_size = []
         else:
@@ -127,51 +89,52 @@ class Critic(nn.Module):
         self.input_size = input_size
         mlp = []
         input_size_ = input_size
-
         for i in range(len(self.hidden_size)):
             mlp.append(nn.Linear(input_size_, self.hidden_size[i]))
             mlp.append(nn.ReLU())
             input_size_ = self.hidden_size[i]
         mlp.append(nn.Linear(self.hidden_size[-1], self.output_size))
-
         self.mlp = nn.Sequential(*mlp)
-
     def forward(self, x):
         x = x.view(x.size(0), -1)
         score = self.mlp(x)
         return score
-
-
-def objectiveW(critic_true, critic_gen, critic_gradpen, lambda_=10.0, device='cuda:0'):
-    # We add a negative to the objective to be able to minimize it instead of maximizing it instead
+def objectiveW(critic_true, critic_gen, critic_gradpen, lambda_=10.0):
+    """
+    Objective function to train the GAN critic on
+    We add a negative to the objective to be able to minimize it instead of maximizing it instead
+    :param critic_true: critic output for true distribution sample
+    :param critic_gen: critic output for "fake" distribution sample
+    :param critic_gradpen: critic output for gradient penalty term
+    :param lambda_: correction factor
+    :return: objective, wasserstein distance estimation
+    """
     critic_true = torch.mean(critic_true)
     critic_gen = torch.mean(critic_gen)
-    critic_gradpen = torch.pow(torch.mean(torch.norm(critic_gradpen, dim=1)-1.0),2)
-    objective = -1.0*critic_true + critic_gen + lambda_*critic_gradpen
+    critic_gradpen = lambda_*torch.mean(torch.pow(critic_gradpen.norm(2, dim=1)-1.0, 2))
+    objective = critic_true - critic_gen - critic_gradpen
     w_d = critic_true - critic_gen
-    return objective, w_d
-
+    return -objective, w_d
 def WassersteinEstimator(true_distrib, gen_distrib, epochs=20):
     """
     Wasserstein distance estimator
     :param true_distrib: ndarray # Already optimal batch size
     :param gen_distrib: ndarray # Already optimal batch size
-    :param lr:
-    :param epochs:
-    :return:
+    :param lr: learning rate
+    :param epochs: training epochs
+    :return: objective and true wasserstein distance estimation
     """
-    # Creating dataset instances
-    critic = Critic(input_size=2, hidden_size=[100, 200, 100])
+    # Creating data set instances
+    critic = Critic(input_size=2, hidden_size=[50, 100, 50])
     critic = critic.to('cuda:0')
     optimizer = SGD(critic.parameters(), lr=0.001)
     objective = 0
     w_d = 0
-
     for epoch in range(epochs):
         optimizer.zero_grad()
         true_tensor = torch.Tensor(next(true_distrib)).to('cuda:0')
         gen_tensor = torch.Tensor(next(gen_distrib)).to('cuda:0')
-        a = torch.Tensor(np.array([random.uniform(0, 1) for _ in range(512)])[:, np.newaxis]).to('cuda:0')
+        a = torch.Tensor(np.array([random.uniform(0, 1.0) for _ in range(512)])[:, np.newaxis]).to('cuda:0')
         true_tensorTemp = true_tensor
         gen_tensorTemp = gen_tensor
         z = torch.mul(a, (true_tensorTemp.detach())) + torch.mul((torch.cuda.FloatTensor(1.0 - a)),(gen_tensorTemp.detach()))
@@ -179,34 +142,25 @@ def WassersteinEstimator(true_distrib, gen_distrib, epochs=20):
         true_pred = critic(true_tensor)
         gen_pred = critic(gen_tensor)
         z_pred = critic(z)
-        grad = torch.autograd.grad(torch.mean(z_pred), z)[0]
-        objective, w_d = objectiveW(true_pred, gen_pred, grad, lambda_=40)
+        grad = torch.autograd.grad(z_pred, z, grad_outputs = torch.ones_like(z_pred).cuda(), retain_graph=True, create_graph=True, only_inputs=True)[0]
+        objective, w_d = objectiveW(true_pred, gen_pred, grad, lambda_=10.0)
         objective.backward()
         optimizer.step()
-
-    return -1.0*objective, w_d
-
-
-
+    return -objective, w_d
 if __name__ == "__main__":
-
-    # 1.3
-    # Model
-
+    # Setting up distributions
     p = iter(samplers.distribution1(0))
     phis = np.arange(-1, 1.1, 0.1)
-
     JS = []
     W = []
-
+    # Getting estimations
     for phi in phis:
         q = iter(samplers.distribution1(phi))
-        JS.append(JensenShannonEstimator(p, q, epochs=1000))
-        W.append(WassersteinEstimator(p, q, epochs=400)[1])
-
+        JS.append(JensenShannonEstimator(p, q, epochs=10000))
+        W.append(WassersteinEstimator(p, q, epochs=10000)[1])
     JS = np.array(JS)
     W = np.array(W)
-
+    # Plotting estimations
     plt.figure()
     plt.title("Estimates for Jensen-Shannon and Wasserstein distance")
     plt.plot(phis, JS, 'r', label="JS")
